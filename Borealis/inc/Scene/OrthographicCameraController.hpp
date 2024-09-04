63b80cb751cdc094f148cc13a80791eefea8e47f
ZyChua2000 <122003260+ZyChua2000@users.noreply.github.com>
2024-09-04 19:47:55 +0800

    test

--- Borealis/inc/Scene/OrthographicCameraController.hpp
+++ Borealis/inc/Scene/OrthographicCameraController.hpp
@@ -23,44 +23,159 @@ namespace Borealis
 {
 	struct OrthographicCameraBounds
 	{
-		float Left, Right, Bottom, Top;
+		float Left, Right, Bottom, Top; // Bounds of the camera
+		/*!***********************************************************************
+			\brief
+				Get the width of the camera bounds
+			\return
+				Width of the camera bounds
+		*************************************************************************/
+		float GetWidth() { return Right - Left; } 
 
-		float GetWidth() { return Right - Left; }
+		/*!***********************************************************************
+			\brief
+				Get the height of the camera bounds
+			\return
+				Height of the camera bounds
+		*************************************************************************/
 		float GetHeight() { return Top - Bottom; }
 	};
 	class OrthographicCameraController
 	{
 	public:
-		OrthographicCameraController(float width, float height, bool rotation = false);
+
+		/*!***********************************************************************
+			\brief
+				Constructor for Orthographic Camera Controller
+			\param[in] width
+				Width of the camera
+			\param[in] height
+				Height of the camera
+			\param[in] rotation
+				Whether the camera can rotate
+		*************************************************************************/
+		OrthographicCameraController(float width, float height, bool rotation = false); 
+
+		/*!***********************************************************************
+			\brief
+				Constructor for Orthographic Camera Controller
+			\param[in] aspectRatio
+				Aspect ratio of the camera
+			\param[in] rotation
+				Whether the camera can rotate
+		*************************************************************************/
 		OrthographicCameraController(float aspectRatio, bool rotation = false);
+
+		/*!***********************************************************************
+			\brief
+				Get the camera
+			\return
+				Orthographic Camera
+		*************************************************************************/
 		OrthographicCamera& GetCamera() { return mCamera; }
+
+		/*!***********************************************************************
+			\brief
+				Get the camera
+			\return
+				Orthographic Camera
+		*************************************************************************/
 		const OrthographicCamera& GetCamera() const { return mCamera;  }
+
+		/*!***********************************************************************
+			\brief
+				Update function for the camera controller
+			\param[in] dt
+				Delta time
+		*************************************************************************/
 		void UpdateFn(float dt);
+
+		/*!***********************************************************************
+			\brief
+				Event function for the camera controller
+			\param[in] e
+				Event
+		*************************************************************************/
 		void EventFn(Event& e);
+
+		/*!***********************************************************************
+			\brief
+				Resize the camera
+			\param[in] width
+				Width of the camera
+			\param[in] height
+				Height of the camera
+		*************************************************************************/
 		void Resize(float width, float height);
 
+		/*!***********************************************************************
+			\brief
+				Get the bounds of the camera
+			\return
+				Orthographic Camera Bounds
+		*************************************************************************/
 		const OrthographicCameraBounds& GetBounds() const { return mBounds; }
+
+		/*!***********************************************************************
+			\brief
+				Set the bounds of the camera
+			\param[in] bounds
+				Orthographic Camera Bounds
+		*************************************************************************/
 		void SetBounds(const OrthographicCameraBounds& bounds) { mBounds = bounds; }
 
+		/*!***********************************************************************
+			\brief
+				Get the Zoom level of the camera
+			\return
+				Zoom level of the camera
+		*************************************************************************/
 		float GetZoomLevel() const { return mZoom; }
+
+		/*!***********************************************************************
+			\brief
+				Set the Zoom level of the camera
+			\param[in] zoom
+				Zoom level of the camera
+		*************************************************************************/
 		void SetZoomLevel(float zoom) { mZoom = zoom; }
 
+		/*!***********************************************************************
+			\brief
+				Get the aspect ratio of the camera
+			\return
+				Aspect ratio of the camera
+		*************************************************************************/
 		float GetAspectRatio() const { return mAspectRatio; }
 	private:
+
+		/*!***********************************************************************
+			\brief
+				Mouse Scrolled Event
+			\param[in] e
+				Mouse Scrolled Event
+		*************************************************************************/
 		bool OnMouseScrolled(EventMouseScrolled& e);
+
+		/*!***********************************************************************
+			\brief
+				Window Resized Event
+			\param[in] e
+				Window Resized Event
+		*************************************************************************/
 		bool OnWindowResized(EventWindowResize& e);	
 
-		bool mRotation;
-		float mAspectRatio;
-		float mZoom;
-		OrthographicCameraBounds mBounds;
-		OrthographicCamera mCamera;
+		bool mRotation; // Whether the camera can rotate
+		float mAspectRatio; // Aspect ratio of the camera
+		float mZoom; // Zoom level of the camera
+		OrthographicCameraBounds mBounds; // Bounds of the camera
+		OrthographicCamera mCamera; // Camera
 		
 
-		glm::vec3 mCameraPosition;
-		float mCameraRotation;
-		float mCameraMoveSpeed;
-		float mCameraRotSpeed;
+		glm::vec3 mCameraPosition; // Position of the camera
+		float mCameraRotation; // Rotation of the camera
+		float mCameraMoveSpeed; // Speed of the camera
+		float mCameraRotSpeed; // Rotation speed of the camera
 	};
 }
 
--- Borealis/inc/Scene/Serialiser.hpp
+++ Borealis/inc/Scene/Serialiser.hpp
@@ -21,15 +21,52 @@ namespace Borealis
 	class Serialiser
 	{
 	public:
+		/*!***********************************************************************
+			\brief
+				Default constructor for Serialiser
+			\param scene
+				The scene to serialise
+		*************************************************************************/
 		Serialiser(const Ref<Scene>& scene);
 
+		/*!***********************************************************************
+			\brief
+				Serialises the scene to a file
+			\param filepath
+				The file path to serialise the scene to
+			\return
+				True if serialisation is successful, false otherwise
+		*************************************************************************/
 		bool SerialiseScene(const std::string& filepath);
+
+		/*!***********************************************************************
+			\brief
+				Deserialises the scene from a file
+			\param filepath
+				The file path to deserialise the scene from
+			\return
+				True if deserialisation is successful, false otherwise
+		*************************************************************************/
 		bool DeserialiseScene(const std::string& filepath);
 
+		/*!***********************************************************************
+			\brief
+				Serialises the editor style to a file
+			\return
+				True if serialisation is successful, false otherwise
+		*************************************************************************/
 		static bool SerialiseEditorStyle();
+
+		/*!***********************************************************************
+			\brief
+				Deserialises the editor style from a file
+			\return
+				True if deserialisation is successful, false otherwise
+		*************************************************************************/
 		static bool DeserialiseEditorStyle();
 	private:
-		Ref<Scene> mScene;
+
+		Ref<Scene> mScene; //!< The scene to serialise
 
 	};
 }
